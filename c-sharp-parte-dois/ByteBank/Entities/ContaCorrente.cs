fd1b5bb49dfb6e6835b64b99755c2749c6026a79
Guilherme Ramos <guimeloramos@hotmail.com>
2018-12-31 12:49:05 -0200

    Adiciona entities e modifica namespace do projeto

---
+++ c-sharp-parte-dois/ByteBank/Entities/ContaCorrente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Entities
+{
+    public class ContaCorrente
+    {
+        public int Id { get; set; }
+        public string Titutlar { get; set; }
+        public int Agencia { get; set; }
+        public int Conta { get; set; }
+        public double Saldo { get; set; }
+    }
+}
--- c-sharp-parte-dois/ByteBank/Program.cs
+++ c-sharp-parte-dois/ByteBank/Program.cs
@@ -1,15 +1,17 @@
-﻿using System;
+﻿using ByteBank.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _01.ByteBank
+namespace ByteBank
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var conta = new ContaCorrente();
         }
     }
 }
